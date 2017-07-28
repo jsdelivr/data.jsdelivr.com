77e45faa487cf7c32d6ba3c4c9e34edae8e95333
Martin Kolárik <martin@kolarik.sk>
2017-07-28 23:19:05 +0200

    Config fix

--- config/local-test.js
+++ config/local-test.js
@@ -3,6 +3,8 @@ module.exports = {
 		debugToken: '4f5dbb6427b186c054465729f5ed0fc6',
 	},
 	db: {
-		database: 'jsdelivr-stats-test',
+		connection: {
+			database: 'jsdelivr-stats-test',
+		},
 	},
 };
