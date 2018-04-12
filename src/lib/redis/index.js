ba24ad3b4af744807c3b21816f73cfa066d4a760
Martin Kolárik <martin@kolarik.sk>
2018-04-12 14:02:11 +0200

    Don't crash when the connection to redis is lost

--- src/lib/redis/index.js
+++ src/lib/redis/index.js
@@ -3,13 +3,7 @@ const redis = require('redis');
 const config = require('config');
 const redisConfig = config.get('redis');
 
-module.exports = redis.createClient({
-	db: redisConfig.db,
-	host: redisConfig.host,
-	port: redisConfig.port,
-	auth_pass: redisConfig.password,
-	detect_buffers: true,
-});
+module.exports = createClient();
 
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
 	module.exports.flushallAsync().catch(() => {});
@@ -34,3 +28,21 @@ redis.RedisClient.prototype.getCompressedAsync = async function (key) {
 redis.RedisClient.prototype.setCompressedAsync = async function (key, value, ...other) {
 	return this.setAsync(key, await zlib.deflateAsync(value), ...other);
 };
+
+function createClient () {
+	let client = redis.createClient({
+		db: redisConfig.db,
+		host: redisConfig.host,
+		port: redisConfig.port,
+		auth_pass: redisConfig.password,
+		detect_buffers: true,
+		enable_offline_queue: false,
+	});
+
+	client
+		.on('ready', () => logger.debug('Connection ready.'))
+		.on('reconnecting', info => logger.debug(_.pick(info, [ 'attempt', 'delay' ], 'Reconnecting.')))
+		.on('error', err => logger.error({ err }, 'Connection error.'));
+
+	return client;
+}
