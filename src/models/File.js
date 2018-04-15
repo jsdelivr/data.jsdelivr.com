c5b82d169e8b5a7d97d31c7ff0666cf50f9776b9
Martin Kolárik <martin@kolarik.sk>
2018-04-15 15:35:04 +0200

    Fix validation rules in File model

--- src/models/File.js
+++ src/models/File.js
@@ -6,7 +6,7 @@ const schema = {
 	packageVersionId: [ Joi.number().integer().min(0).required(), Joi.string().regex(/^@/) ],
 	filename: Joi.string().max(255).required(),
 	sha256: Joi.binary().length(32).required().allow(null),
-	fetchAttemptsLeft: Joi.number().integer().min(0).max(128).required(),
+	fetchAttemptsLeft: Joi.number().integer().min(0).max(128).required().allow(null),
 };
 
 class File extends BaseModel {
