7faf34e387abe3ccc37e9401832ec40a767ab67b
Martin Kolárik <martin@kolarik.sk>
2019-06-05 21:31:39 +0200

    Use new WithLock methods where appropriate

--- src/routes/lib/v1/StatsRequest.js
+++ src/routes/lib/v1/StatsRequest.js
@@ -8,19 +8,16 @@ const Logs = require('../../../models/Logs');
 const dateRange = require('../../utils/dateRange');
 const sumDeep = require('../../utils/sumDeep');
 
-const PromiseLock = require('../../../lib/promise-lock');
-const promiseLock = new PromiseLock('st');
-
 class StatsRequest extends BaseRequest {
 	async handleNetwork () {
-		this.ctx.body = await promiseLock.get(`network:${JSON.stringify(this.dateRange)}`, () => this.handleNetworkInternal(relativeDayUtc(1)), undefined, true);
+		this.ctx.body = await this.handleNetworkInternal(relativeDayUtc(1));
 		this.setCacheHeader();
 	}
 
 	async handleNetworkInternal (redisCacheExpirationDate) {
-		let fileHits = await PackageHits.get(undefined, redisCacheExpirationDate).getSumPerDate(...this.dateRange);
-		let otherHits = await OtherHits.get(undefined, redisCacheExpirationDate).getSumPerDate(...this.dateRange);
-		let datesTraffic = await Logs.get(undefined, redisCacheExpirationDate).getMegabytesPerDate(...this.dateRange);
+		let fileHits = await PackageHits.getWithLock(undefined, redisCacheExpirationDate).getSumPerDate(...this.dateRange);
+		let otherHits = await OtherHits.getWithLock(undefined, redisCacheExpirationDate).getSumPerDate(...this.dateRange);
+		let datesTraffic = await Logs.getWithLock(undefined, redisCacheExpirationDate).getMegabytesPerDate(...this.dateRange);
 		let sumFileHits = sumDeep(fileHits);
 		let sumOtherHits = sumDeep(otherHits);
 
@@ -40,7 +37,7 @@ class StatsRequest extends BaseRequest {
 				total: sumDeep(datesTraffic),
 				dates: dateRange.fill(datesTraffic, ...this.dateRange),
 			},
-			meta: await Logs.get(undefined, redisCacheExpirationDate).getMetaStats(...this.dateRange),
+			meta: await Logs.getWithLock(undefined, redisCacheExpirationDate).getMetaStats(...this.dateRange),
 		};
 
 		if (!result.meta.records) {
@@ -55,12 +52,12 @@ class StatsRequest extends BaseRequest {
 	}
 
 	async handlePackages () {
-		this.ctx.body = await promiseLock.get(`packages:${JSON.stringify(this.dateRange)}:${JSON.stringify(this.pagination)}`, () => this.handlePackagesInternal(relativeDayUtc(1)), undefined, true);
+		this.ctx.body = await this.handlePackagesInternal(relativeDayUtc(1));
 		this.setCacheHeader();
 	}
 
 	async handlePackagesInternal (redisCacheExpirationDate) {
-		return Package.get(undefined, redisCacheExpirationDate).getTopPackages(...this.dateRange, ...this.pagination);
+		return Package.getWithLock(undefined, redisCacheExpirationDate).getTopPackages(...this.dateRange, ...this.pagination);
 	}
 }
 
