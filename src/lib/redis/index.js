209387e82ac110fc8f0bfb63d6c4adc8978bae95
Martin Kolárik <martin@kolarik.sk>
2019-06-05 21:33:57 +0200

    Optimize redis compression

--- src/lib/redis/index.js
+++ src/lib/redis/index.js
@@ -22,12 +22,22 @@ redis.RedisClient.prototype.getCompressedAsync = async function (key) {
 
 	if (!value) {
 		return value;
+	} else if (value[0] === 0xFF) {
+		return value.toString('utf8', 1);
 	}
 
 	return (await zlib.inflateAsync(value)).toString();
 };
 
 redis.RedisClient.prototype.setCompressedAsync = async function (key, value, ...other) {
+	value = String(value);
+
+	// Don't compress if the data is less than 1024 bytes.
+	if (value.length < 1024) {
+		// Add a 0xFF byte as a flag for "not compressed".
+		return this.setAsync(key, '\xFF' + value, ...other);
+	}
+
 	return this.setAsync(key, await zlib.deflateAsync(value), ...other);
 };
 
