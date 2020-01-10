dfe47956186fbb90baf08733a3af7031c322e321
Martin Kolárik <martin@kolarik.sk>
2020-01-10 15:14:57 +0100

    Update 20200107165603_v1.9.3.js

--- migrations/20200107165603_v1.9.3.js
+++ migrations/20200107165603_v1.9.3.js
@@ -3,7 +3,7 @@ const sql = fs.readFileSync(__filename + '.sql', 'utf8');
 
 exports.up = async (db) => {
 	await db.schema.createTable('package_version_hits', (table) => {
-		table.integer('packageVersionId').unsigned().notNullable();
+		table.integer('packageVersionId').unsigned().references('id').inTable('package_version').onUpdate('cascade').onDelete('cascade');
 		table.date('date').index();
 		table.integer('hits').unsigned().defaultTo(0).notNullable();
 		table.specificType('bandwidth', 'float').unsigned().defaultTo(0).notNullable();
