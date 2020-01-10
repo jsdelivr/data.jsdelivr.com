88bd9581248f8efb07b831543603de557fb13c2c
Igor Cherkasov <cherkasov.i@perfops.net>
2020-01-10 11:33:33 +0100

    JD-5 gh-54 package version hits stats

---
+++ src/models/PackageVersionHits.js
@@ -0,0 +1,48 @@
+const Joi = require('joi');
+const BaseCacheModel = require('./BaseCacheModel');
+
+const schema = {
+	packageVersionId: Joi.number().integer().min(0).required().allow(null),
+	date: Joi.date().required(),
+	hits: Joi.number().integer().min(0).required(),
+	bandwidth: Joi.number().min(0).required(),
+};
+
+class PackageVersionHits extends BaseCacheModel {
+	static get table () {
+		return 'package_version_hits';
+	}
+
+	static get schema () {
+		return schema;
+	}
+
+	static get unique () {
+		return [ 'packageVersionId', 'date' ];
+	}
+
+	constructor (properties = {}) {
+		super();
+
+		/** @type {number} */
+		this.packageVersionId = null;
+
+		/** @type {Date} */
+		this.date = null;
+
+		/** @type {number} */
+		this.hits = 0;
+
+		/** @type {number} */
+		this.bandwidth = 0;
+
+		Object.assign(this, properties);
+		return new Proxy(this, BaseCacheModel.ProxyHandler);
+	}
+
+	toSqlFunctionCall () {
+		return db.raw(`select updateOrInsertPackageVersionHits(@lastIdPackageVersion, ?, ?, ?);`, [ this.date, this.hits, this.bandwidth ]);
+	}
+}
+
+module.exports = PackageVersionHits;
