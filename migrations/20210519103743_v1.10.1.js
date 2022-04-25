2b49435254b4179721f2cc47155dde7655cedfe3
Martin Kolárik <martin@kolarik.sk>
2022-04-25 18:04:50 +0200

    Make processAttemptsLeft unsigned

--- migrations/20210519103743_v1.10.1.js
+++ migrations/20210519103743_v1.10.1.js
@@ -1,6 +1,6 @@
 exports.up = async (db) => {
 	await db.schema.alterTable('log_file', (table) => {
-		table.specificType('processAttemptsLeft', 'tinyint').defaultTo(10).notNullable().index();
+		table.specificType('processAttemptsLeft', 'tinyint').unsigned().defaultTo(10).notNullable().index();
 	});
 };
 
