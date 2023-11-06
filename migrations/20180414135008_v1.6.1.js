19e2a1cc499220a531d57ea751a3dcf8ad155266
Martin Kolárik <martin@kolarik.sk>
2023-11-06 12:08:48 +0100

    Change default fetchAttemptsLeft to 5

--- migrations/20180414135008_v1.6.1.js
+++ migrations/20180414135008_v1.6.1.js
@@ -2,7 +2,7 @@ exports.up = async (db) => {
 	await db.schema.raw('SET @@foreign_key_checks = 0;');
 
 	await db.schema.alterTable('file', (table) => {
-		table.specificType('fetchAttemptsLeft', 'tinyint').defaultTo(3).notNullable().index();
+		table.specificType('fetchAttemptsLeft', 'tinyint').defaultTo(5).notNullable().index();
 	});
 
 	await db.schema.raw('SET @@foreign_key_checks = 1;');
