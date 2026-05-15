9698a6b4daab732b96dc5cdae087fd381f4dbc36
Martin Kolárik <martin@kolarik.sk>
2026-05-16 00:22:25 +0200

    Update hooks.js

--- test/hooks.js
+++ test/hooks.js
@@ -1,21 +1,18 @@
-import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 import nock from 'nock';
 import chai from 'chai';
 import config from 'config';
+import expectAssert from 'expect-assert';
+import chaiHttp from 'chai-http';
+import chaiOas from './plugins/oas/index.js';
+import chaiSnapshot from './plugins/snapshot/index.js';
 
 const serverConfig = config.get('server');
 global.server = `http://127.0.0.1:${serverConfig.port}`;
-
-import expectAssert from 'expect-assert';
 chai.expect = expectAssert(chai.expect);
 
-import chaiHttp from 'chai-http';
 chai.use(chaiHttp);
-
-import chaiOas from './plugins/oas/index.js';
-import chaiSnapshot from './plugins/snapshot/index.js';
 
 global.chaiSnapshotInstance = chaiSnapshot({
 	snapshotResponses: !!Number(process.env.SNAPSHOT_RESPONSES),
@@ -27,7 +24,7 @@ chai.use(chaiSnapshotInstance);
 export const mochaHooks = {
 	async beforeAll () {
 		global.log = logger.scope('test');
-		chai.use(await chaiOas({ specPath: dirname(fileURLToPath(import.meta.url)) + '/../src/public/v1/spec.yaml' }));
+		chai.use(await chaiOas({ specPath: fileURLToPath(new URL('../src/public/v1/spec.yaml', import.meta.url)) }));
 
 		if (global.v8debug === undefined && !/--debug|--inspect/.test(process.execArgv.join(' ')) && !process.env.JB_IDE_PORT) {
 			let { default: blocked } = await import('blocked');
