982d4e1e9eaa2af2b0540c98dcf94b196a345023
Martin Kolárik <martin@kolarik.sk>
2026-05-17 15:48:51 +0200

    Update setup-dev.js

--- test/setup-dev.js
+++ test/setup-dev.js
@@ -1,8 +1,8 @@
 process.env.NODE_ENV = 'development';
 
-import '../src/lib/startup.js';
-import setupDb from './setup-db.js';
-
 (async () => {
+	await import('../src/lib/startup.js');
+	let { default: setupDb } = await import('./setup-db.js');
+
 	await setupDb({ databaseDate: new Date().toISOString().substr(0, 10) });
 })().then(() => process.exit()).catch(console.error);
