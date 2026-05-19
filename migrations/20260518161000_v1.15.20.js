cba0ef8dbdbd5df06b03bf7501f7d5e9e7b6758e
Martin Kolárik <martin@kolarik.sk>
2026-05-19 18:07:43 +0200

    Merge migrations, reduce compressed tables

--- migrations/20260518161000_v1.15.20.js
+++ migrations/20260518161000_v1.15.20.js
@@ -3,12 +3,13 @@ import Bluebird from 'bluebird';
 const compressedTables = [
 	'package_listing',
 	'package_entrypoints',
+	'view_top_packages',
 ];
 
 export const up = async (db) => {
 	await Bluebird.map(compressedTables, async (table) => {
 		await db.schema.raw(`alter table \`${table}\` row_format = dynamic, page_compressed = 1`);
-	}, { concurrency: 4 });
+	});
 };
 
 export const down = () => {};
