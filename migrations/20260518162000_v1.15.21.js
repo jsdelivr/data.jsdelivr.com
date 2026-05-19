1970b1ca06f7dc3df985fa43bddd3644cc519513
Martin Kolárik <martin@kolarik.sk>
2026-05-19 16:00:54 +0200

    Update 20260518162000_v1.15.21.js

--- migrations/20260518162000_v1.15.21.js
+++ migrations/20260518162000_v1.15.21.js
@@ -21,8 +21,6 @@ const compressedTables = [
 ];
 
 export const up = async (db) => {
-	await db.schema.raw(`set global innodb_compression_algorithm = 'lz4'`);
-
 	await Bluebird.map(compressedTables, async (table) => {
 		await db.schema.raw(`alter table \`${table}\` row_format = dynamic, page_compressed = 1`);
 	}, { concurrency: 4 });
