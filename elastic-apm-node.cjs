a32596ca8f9a4fa75cbaabdd8fbffe60dbe7ae05
Martin Kolárik <martin@kolarik.sk>
2026-06-12 14:47:29 +0200

    Remove errorOnAbortedRequests

--- elastic-apm-node.cjs
+++ elastic-apm-node.cjs
@@ -10,7 +10,5 @@ module.exports = {
 	captureSpanStackTraces: false,
 	captureErrorLogStackTraces: 'always',
 	ignoreUrls: [ '/favicon.ico', '/heartbeat', '/amp_preconnect_polyfill_404_or_other_error_expected._Do_not_worry_about_it' ],
-	errorOnAbortedRequests: true,
-	abortedErrorThreshold: 30,
 	transactionSampleRate: .1,
 };
